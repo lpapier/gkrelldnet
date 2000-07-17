1bfdd2a26e4166464d7fcbdfef59dc101444e00e
Laurent Papier <papier[at]tuxfan.net>
2000-07-17 19:20:22 +0000

    First snapshot

---
+++ gkrelldnet.h
@@ -0,0 +1,16 @@
+
+#ifndef __SENSORS_H__
+#define __SENSORS_H__
+
+#include <gkrellm/gkrellm.h>
+
+#define	CONFIG_NAME	"Distributed.net"  /* Name in the configuration window */
+#define CONFIG_KEYWORD "dnet"
+#define	STYLE_NAME	"dnet"		  /* Theme subdirectory name and gkrellmrc */
+
+#define LOCATION MON_CPU
+
+#define DNET_MAJOR_VERSION 0
+#define DNET_MINOR_VERSION 1
+
+#endif

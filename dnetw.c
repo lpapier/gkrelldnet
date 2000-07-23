36f1c86281c400069d9035d8fa54dcdd1295517d
Laurent Papier <papier[at]tuxfan.net>
2000-07-23 15:37:06 +0000

    Some code cleanup, and more documentation.

--- dnetw.c
+++ dnetw.c
@@ -1,3 +1,22 @@
+/* dnetw: a distributed.net client wrapper
+|  Copyright (C) 2000 Laurent Papier
+|
+|  Author:  Laurent Papier    papier@linuxfan.com
+|
+|  This program is free software which I release under the GNU General Public
+|  License. You may redistribute and/or modify this program under the terms
+|  of that license as published by the Free Software Foundation; either
+|  version 2 of the License, or (at your option) any later version.
+|
+|  This program is distributed in the hope that it will be useful,
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+|  GNU General Public License for more details.
+| 
+|  To get a copy of the GNU General Puplic License, write to the Free Software
+|  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
